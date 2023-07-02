9692911e8a78f6c933a1f9179f02fc234974bfd1
zhangtienyong <tienhanprab@gmail.com>
2023-07-02 23:56:04 +0800

    for loops

--- independent-study/smartcontract/contracts/MyContract.sol
+++ independent-study/smartcontract/contracts/MyContract.sol
@@ -11,4 +11,46 @@ contract MyContract {
         myNumber = _number;
         myWords = _words;
     }
+
+        // Use for-loop to iterate over elements
+    function iterate(uint256 iterations) public pure returns(uint256) {
+        uint256 totalIterations = 0;
+    
+        for (uint256 i = 0; i < iterations; i++) {
+            totalIterations++;
+        }
+
+        return totalIterations;
+    }
+
+    // Summing Array Elements
+    function sumArray(uint256[] memory numbers) public pure returns (uint256) {
+        uint256 sum = 0;
+
+        for(uint256 i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+
+    // Emitting Events for Array Elements
+    event ElementProcessed(uint256 element);
+
+    function processArray(uint256[] memory numbers) public {
+        for(uint256 i = 0; i < numbers.length; i++) {
+            emit ElementProcessed(numbers[i]);
+        }
+    }
+
+    //Dynamic Array Initialization
+    function createDynamicArray(uint256 size) public pure returns (uint256[] memory) {
+        uint256[] memory dynamicArray = new uint256[](size);
+
+        for (uint256 i = 0; i < size; i++) {
+            dynamicArray[i] = i;
+        }
+
+        return dynamicArray;
+  }
 }
